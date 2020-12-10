1865ebda17049a9c1fccf8c3e5133d59f8aeebb2
adrien <adrichristophe@gmail.com>
2020-12-10 10:34:28 +0100

    version fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,53 +1,20 @@
 <?php
 
 namespace App\DataFixtures;
-use Faker\Factory;
-use App\Entity\Role;
-use App\Entity\User;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class AppFixtures extends Fixture
 {
-    private $encoder;
-
-    public function __construct(UserPasswordEncoderInterface $encoder)
-    {
-        $this->encoder = $encoder;
-    }
-
     public function load(ObjectManager $manager)
     {
-        $faker = Factory::create('fr_FR');
-
-        $adminRole = new Role();
-        $adminRole->setTitle('ROLE_ADMIN');
-        $manager->persist($adminRole);
-
-        
-
-        // Nous gérons les utilisateurs
-        $users = [];
-        for ($i = 1; $i <= 10; $i++) {
-            $user = new User();
-            $hash = $this->encoder->encodePassword($user, 'password');
-
-            $user->setFirstName($faker->firstname($genre))
-                ->setLastName($faker->lastname)
-                ->setEmail($faker->email)
-                ->setIntroduction($faker->sentence())
-                ->setHash($hash);
-                
-
-            $manager->persist($user);
-            $users[] = $user;
-        
-
-        
-
-            
-        }
+        // $product = new Product();
+        // $manager->persist($product);
+        $product = new Product();
+        $manager->persist($product);
 
         $manager->flush();
     }
