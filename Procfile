3ca6a483cad7b4af9e0ad56e5c195b8c1b9c136d
adrien <adrichristophe@gmail.com>
2020-12-10 14:24:09 +0100

    version avec fixtures

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release : php bin/console cache:clear && php bin/console cache:warmup && php bin/console doctrine:migrations:migrate 
+release : php bin/console cache:clear && php bin/console doctrine:migrations:migrate && php bin/console doctrine:fixtures:load --no-interaction --env=PROD 
 web: heroku-php-apache2 public/
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $manager->persist($adminRole);
+        $adminUser = new User();
+        $adminUser->setFirstName('Adrien')
+            ->setLastName('Christophe')
+            ->setEmail('adrichristophe@gmail.com')
+            ->setPassword('M0tdep@sse')
+            ->setHash($this->encoder->encodePassword($adminUser, 'password'));
+        $manager->persist($adminUser);
+        $manager->flush();
+    }
+}
