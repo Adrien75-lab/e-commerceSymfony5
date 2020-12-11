d181626ba47c3ab4cf092331cbc078c97f5ec664
adrien <adrichristophe@gmail.com>
2020-12-11 13:45:58 +0100

    version finale

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
+    .addStyleEntry('css/app','./assets/css/app.css')
     
-    .addStyleEntry('css/boutiquefrancaise', './assets/css/boutiquefrancaise.css')
-    //.addStyleEntry('css', './assets/css/carousel.css')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     
