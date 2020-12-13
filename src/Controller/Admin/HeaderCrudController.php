b55ac0b8dd528116a86020078bba6a73992c2330
adrien <adrichristophe@gmail.com>
2020-12-13 18:56:17 +0100

    version finale

--- src/Controller/Admin/HeaderCrudController.php
+++ src/Controller/Admin/HeaderCrudController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Admin;
 use App\Entity\Header;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Controller\Admin\HeaderCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
@@ -29,13 +28,13 @@ class HeaderCrudController extends AbstractCrudController
     {
         return [
             TextField::new('title','Titre du header'),
-            SlugField::new('slug')->setTargetFieldName('name'),
             TextareaField::new('content',' contenu de notre header'),
+            SlugField::new('slug')->setTargetFieldName('name'),
             TextField::new('btnTitle','Titre de notre bouton'),
             TextField::new('btnUrl','Url de destination de notre bouton'),
             ImageField::new('illustration')
-            ->setBasePath('uploads')
-            ->setUploadDir('public/uploads')
+            ->setBasePath('uploads/')
+            ->setUploadDir('./public/uploads')
 
             ->setUploadedFileNamePattern('[randomhash].[extension]')
             ->setRequired(false),
