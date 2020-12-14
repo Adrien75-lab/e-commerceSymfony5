c2998a62c6ec1aa4eb6abb21b11a56e58147aa04
adrien <adrichristophe@gmail.com>
2020-12-14 08:38:02 +0100

    version finale

--- src/Controller/Admin/HeaderCrudController.php
+++ src/Controller/Admin/HeaderCrudController.php
@@ -33,14 +33,14 @@ class HeaderCrudController extends AbstractCrudController
             TextField::new('btnTitle','Titre de notre bouton'),
             TextField::new('btnUrl','Url de destination de notre bouton'),
             ImageField::new('illustration')
-            ->setBasePath('uploads/')
+            
             ->setUploadDir('public/uploads')
 
             ->setUploadedFileNamePattern('[randomhash].[extension]')
             ->setRequired(false),
 
         ];
-        
+
         $this->entityManager->flush();
     }
     
